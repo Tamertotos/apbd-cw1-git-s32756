1615c8eb9dc6b8b9f4e2fe989388b31935cbb4b8
Tamertotos <tamercantotos@gmail.com>
2026-03-15 14:51:06 +0100

    "Another line before rebase in main branch"

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -23,6 +23,7 @@ class Program
         int[] x = { 3, 4 };
         Console.WriteLine("Average value of the array is: " + CalculateAverage(x));
         Console.WriteLine("The maximum value in the array is: " + CalculateMax(x));
+        Console.WriteLine("Another line before rebase in main branch");
     }
 
     static int CalculateAverage(int[] values)
